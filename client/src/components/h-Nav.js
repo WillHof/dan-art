150fa82db4cfe72638de6f7c7e466c39d0af9516
Will Hofstadter <whof@bu.edu>
2020-02-27 16:06:30 -0500

    styling vNav

--- client/src/components/h-Nav.js
+++ client/src/components/h-Nav.js
@@ -7,8 +7,8 @@ export class Nav extends Component {
                 <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
 
                     <a className="navbar-brand" href="/">Dan's Art</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
--- client/src/components/v-Nav.js
+++ client/src/components/v-Nav.js
@@ -4,40 +4,36 @@ export class Vnav extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <nav className="navbar navbar-light bg-white" id="VNav">
+                    <a className="navbar-brand" id="nBrand" href="/">Dan's Art</a>
 
-                    <a className="navbar-brand" href="/">Dan's Art</a>
-
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <li className={window.location.pathname === "" ? "nav-item active" : "nav-item"}>
-                                <Link to="/" className="nav-link">
-                                    Home
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0" id="">
+                        <li className={window.location.pathname === "" ? "nav-item active" : "nav-item"}>
+                            <Link to="/" className="nav-link">
+                                Home
                                 </Link>
-                            </li>
-                            <li className={window.location.pathname === "/Paintings" ? "nav-item active" : "nav-item"}>
-                                <Link to="/Paintings" className="nav-link">
-                                    Work
+                        </li>
+                        <li className={window.location.pathname === "/Paintings" ? "nav-item active" : "nav-item"}>
+                            <Link to="/Paintings" className="nav-link">
+                                Work
                                 </Link>
-                            </li>
-                            <li className={window.location.pathname === "/About" ? "nav-item active" : "nav-item"}>
-                                <Link to="/About" className="nav-link">
-                                    About the Artist
+                        </li>
+                        <li className={window.location.pathname === "/About" ? "nav-item active" : "nav-item"}>
+                            <Link to="/About" className="nav-link">
+                                About the Artist
                                 </Link>
-                            </li>
-                            <li className={window.location.pathname === "/Writing" ? "nav-item active" : "nav-item"}>
-                                <Link to="/Writing" className="nav-link">
-                                    Writing
+                        </li>
+                        <li className={window.location.pathname === "/Writing" ? "nav-item active" : "nav-item"}>
+                            <Link to="/Writing" className="nav-link">
+                                Writing
                                 </Link>
-                            </li>
-                            <li className={window.location.pathname === "/Contact" ? "nav-item active" : "nav-item"}>
-                                <Link to="/Contact" className="nav-link">
-                                    Contact
+                        </li>
+                        <li className={window.location.pathname === "/Contact" ? "nav-item active" : "nav-item"}>
+                            <Link to="/Contact" className="nav-link">
+                                Contact
                                 </Link>
-                            </li>
-                        </ul>
-
-                    </div>
+                        </li>
+                    </ul>
                 </nav>
             </div>
         )
