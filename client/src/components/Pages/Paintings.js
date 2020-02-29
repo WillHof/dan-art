6f66c8431f1c0a878d7fa780affe9319ace7ce6c
Will <whof@bu.edu>
2020-02-29 16:12:02 -0500

    parsing filenames for painting info instead of DB connection/maintenance. painting info displays on painting page

--- client/src/components/Pages/Paintings.js
+++ client/src/components/Pages/Paintings.js
@@ -4,17 +4,30 @@ import Vnav from "../v-Nav"
 export class Paintings extends Component {
     state = {
         images: [],
-        type: "Large Abstractions"
+        type: "Large Abstractions",
+        names: [],
+        dims: [],
     }
     importAll = (r) => {
         return r.keys().map(r);
     }
     componentDidMount() {
-        let paths = require.context("../../Large Abstractions", false, /\.(png|jpe?g|svg)$/);
+        let paths = this.importAll(require.context("../../Large Abstractions", false, /\.(png|jpe?g|svg)$/));
+        let namePath = /[\w-]+;/;
+        let dimPath = /[\dx\d]+\./;
+        let pArr = [];
+        let dArr = [];
+        for (let index = 0; index < paths.length; index++) {
+            const element = paths[index];
+            let pName = element.match(namePath).pop();
+            let pDims = element.match(dimPath).pop();
+            pArr.push(pName.substring(0, (pName.length - 1)));
+            dArr.push(pDims.substring(0, (pDims.length - 1)));
+        }
         //combines path array with nonexistent array of objects to contain info about the paintings
         // for (let index = 0; index < paths.length; index++) {
         //     Object.assign(obj1[i],...paths[i])}
-        this.setState({ images: this.importAll(paths) });
+        this.setState({ images: paths, names: pArr, dims: dArr });
     }
     render() {
         return (
@@ -28,8 +41,13 @@ export class Paintings extends Component {
                             <Nav />
                         </div>
                         <div className="col-md-6 col-sm-12 blockContainer mr-auto ml-auto">
-                            {this.state.images.map(danPainting => (
-                                <img src={danPainting} alt={danPainting.id} key={danPainting} className="paintingImage"></img>
+                            {this.state.images.map((danPainting, index) => (
+                                <div key={index}>
+                                    <img src={danPainting} alt={danPainting.id} key={danPainting} className="paintingImage"></img>
+                                    <div className="row">
+                                        <div className="col-12 garamond h6 text-center mb-4 mt-2" key={index}><b>{this.state.names[index]} {this.state.dims[index]}</b></div>
+                                    </div>
+                                </div>
                             ))}
                         </div>
                         <div className="col-md-2 d-none d-sm-block"></div>
--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
 
-const api = require("../controllers/apicontroller")
-router.route("/api/paintingData")
-    .get(api.Example)
+// const api = require("../controllers/apicontroller")
+// router.route("/api/paintingData")
+//     .get(api.Example)
 
 module.exports = router
