97ff7cc7fe57474e59062bc2d5557763c9e0f6b5
Will Hofstadter <whof@bu.edu>
2020-04-04 04:25:05 -0400

    added landing page. site skeleton is done.

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
+import homeBG from "../../assets/Home/DH home page art B.jpg"
 export class Home extends Component {
     render() {
         return (
             <div>
-                <div className="container">
-                    home
+                <div className="container-fluid no-m">
+                    <div className="home-wrapper">
+                        <div className="fp-wrapper">
+                            <div className="home-wrapper">
+                                <img src={homeBG} alt="home" id="home" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="home-wrapper">
+                        <div className="tx-wrapper">
+                            <div className="title-group">
+                                <div><Link to="/Paintings?category=SmallAbstractions" id="danTitle" className="baloo">DAN HOFSTADTER</Link></div>
+                                <div> <Link to="/Paintings?category=SmallAbstractions" id="danSub" className="baloo"><i>paintings</i></Link></div>
+
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         )
