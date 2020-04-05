c04da9c828659aa0f7ba37768c07afedb5b137bf
Will Hofstadter <whof@bu.edu>
2020-04-04 20:08:30 -0400

    set up email server with test data with nodemailer

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -1,11 +1,30 @@
-const axios = require("axios")
-// const csv = require("jquery-csv")
-const serveIndex = require('serve-index')
-const path = require("path");
-const fs = require('fs')
-
+const axios = require("axios");
+const nodemailer = require("nodemailer");
+require('dotenv').config()
 module.exports = {
-    Example: function (req, res) {
+    email: function (req, res) {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'thehorrorofkurtz@gmail.com',
+                pass: process.env.gPass
+            }
+        });
+        const mailOptions = {
+            from: 'danSite@noreply.com',
+            to: 'whof@bu.edu',
+            subject: 'testing subject',
+            text: 'body here'
+        };
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                console.log(err)
+                res.status(503)
+            } else {
+                console.log('Email sent:' + info.response)
+                res.status(200)
+            }
+        })
         console.log(req.body)
     }
 }
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path')
 const routes = require("./routing/apiroutes");
+require('dotenv').config()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(routes)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
