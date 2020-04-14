76b5c2c759bca7f6b8ceccff5e75169c5c50eb77
Will Hofstadter <whof@bu.edu>
2020-04-14 01:08:09 -0400

    added slideshow content styles to contact page.

--- client/src/components/Pages/Contact.js
+++ client/src/components/Pages/Contact.js
@@ -3,6 +3,7 @@ import Nav from "../h-Nav"
 import Vnav from "../v-Nav"
 import painting from "../../assets/LargeAbstractions/DH1;50x58.jpeg"
 import EmailForm from "../EmailForm"
+import SlideShow from "../SlideShow"
 export class Contact extends Component {
 
 
@@ -19,13 +20,15 @@ export class Contact extends Component {
                         </div>
                         <div className="col-md-6 col-sm-12  blockContainer mr-auto ml-auto">
                             <div id="slideShow">
-                                <img src={painting} alt="Painting" id="aboutImage" />
+                                <SlideShow />
+                                {/* <img src={painting} alt="Painting" id="aboutImage" /> */}
                             </div>
                             <div className="row">
                                 <div className="col-12 mt-3 mb-3 baskerville">
                                     Those wishing to inquire about prices or to purchase pieces should use the form below for a prompt response.
                                 </div>
                             </div>
+
                             <EmailForm />
                         </div>
                         <div className="col-md-2 d-none d-sm-block"></div>
--- client/src/components/SlideShow.js
+++ client/src/components/SlideShow.js
@@ -1,9 +1,87 @@
 import React, { Component } from 'react'
 
+import EarlyWork from "../assets/EarlyWork/index";
+import RecentWork from "../assets/RecentWork/index";
+import SmallAbstractions from "../assets/SmallAbstractions/index";
+import LargeAbstractions from "../assets/LargeAbstractions/index";
+import MonotypesDrawings from "../assets/MonotypesDrawings/index";
+import InstallationViews from "../assets/InstallationViews/index";
+
 export class SlideShow extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: "",
+            names: "",
+            dims: "",
+            index: 0
+        }
+    }
+    componentDidMount() {
+        this.getPaintings("SmallAbstractions")
+    }
+    getPaintings = (category) => {
+        let imagePaths = []
+        if (category === "EarlyWork") {
+            imagePaths = EarlyWork;
+        };
+        if (category === "LargeAbstractions") {
+            imagePaths = LargeAbstractions
+        };
+        if (category === "SmallAbstractions") {
+            imagePaths = SmallAbstractions
+        };
+        if (category === "MonotypesDrawings") {
+            imagePaths = MonotypesDrawings
+        };
+        if (category === "InstallationViews") {
+            imagePaths = InstallationViews
+        };
+        if (category === "RecentWork") {
+            imagePaths = RecentWork
+        }
+
+        let namePath = /(\w|(\w+\s)*)+;/;
+        let dimPath = /[\d.*x\d]+\./;
+        let pArr = [];
+        let dArr = [];
+        if (imagePaths.length) {
+            for (let index = 0; index < imagePaths.length; index++) {
+                const element = imagePaths[index];
+                let pName = element.match(namePath);
+                let pDims = element.match(dimPath);
+                pArr.push(pName[0].substring(0, (pName[0].length - 1)));
+                dArr.push(pDims[0].substring(0, (pDims[0].length - 1)));
+            }
+            this.setState({ images: imagePaths, names: pArr, dims: dArr })
+        }
+        else {
+            this.setState({ images: "", names: "", dims: "" })
+        }
+    }
     render() {
         return (
             <div>
+                <div className="relative">
+                    <div className="arrowContainerL">
+                        <div id="leftArrow">
+                            <span className="carousel-control-prev-icon"></span>
+                        </div>
+                    </div>
+                    <div className="arrowContainerR">
+                        <div id="rightArrow">
+                            <span className="carousel-control-next-icon"></span>
+                        </div>
+                    </div>
+
+                    <div className="stuff">
+                        <img src={this.state.images[this.state.index]} id="aboutImage" className="mr-auto ml-auto" alt={this.state.names[this.state.index]} />
+                        <div className="row">
+                            <div className="col-12 text-right mb-4 h6 tgray pDesc">{this.state.names[this.state.index]} {this.state.dims[this.state.index]}</div>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
         )
