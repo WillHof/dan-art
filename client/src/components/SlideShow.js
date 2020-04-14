b7acf8c42417a01867f8172849b3f427248e108a
Will Hofstadter <whof@bu.edu>
2020-04-14 01:35:43 -0400

    cycling through images in slideshow works

--- client/src/components/SlideShow.js
+++ client/src/components/SlideShow.js
@@ -16,6 +16,7 @@ export class SlideShow extends Component {
             dims: "",
             index: 0
         }
+        this.onClick = this.onClick.bind(this)
     }
     componentDidMount() {
         this.getPaintings("SmallAbstractions")
@@ -59,30 +60,42 @@ export class SlideShow extends Component {
             this.setState({ images: "", names: "", dims: "" })
         }
     }
+    onClick(e) {
+        e.preventDefault()
+        let x = this.state.index + Number(e.target.getAttribute('value'))
+        console.log(x < 0)
+
+        if (x < 0) {
+            this.setState({ index: this.state.images.length - 1 })
+        }
+        if (x > this.state.images.length - 1) {
+            this.setState({ index: 0 })
+        }
+        else {
+            this.setState({ index: x })
+        }
+    }
     render() {
         return (
             <div>
                 <div className="relative">
-                    <div className="arrowContainerL">
-                        <div id="leftArrow">
-                            <span className="carousel-control-prev-icon"></span>
+                    <div className="arrowContainerL" value={-1} onClick={this.onClick}>
+                        <div id="leftArrow" value={-1}>
+                            <span className="carousel-control-prev-icon" value={-1}></span>
                         </div>
                     </div>
-                    <div className="arrowContainerR">
-                        <div id="rightArrow">
-                            <span className="carousel-control-next-icon"></span>
+                    <div className="arrowContainerR" value={1} onClick={this.onClick}>
+                        <div id="rightArrow" value={1}>
+                            <span className="carousel-control-next-icon" value={1}></span>
                         </div>
                     </div>
-
                     <div className="stuff">
                         <img src={this.state.images[this.state.index]} id="aboutImage" className="mr-auto ml-auto" alt={this.state.names[this.state.index]} />
                         <div className="row">
                             <div className="col-12 text-right mb-4 h6 tgray pDesc">{this.state.names[this.state.index]} {this.state.dims[this.state.index]}</div>
                         </div>
                     </div>
-
                 </div>
-
             </div>
         )
     }
