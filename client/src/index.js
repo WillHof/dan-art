a6254099507711b3235a4bcfec9b7e44fc629758
Will Hofstadter <whof@bu.edu>
2020-04-18 04:58:26 -0400

    minimally integrated React Bootstrap such that it, jquery, and popper get bundled by the webpack, eliminating css style inheritance lag when switching breakpoints

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery';
+import 'popper.js/dist/umd/popper';
+import 'bootstrap/dist/js/bootstrap';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(routes)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 app.listen(PORT, () => console.log("app listening at http://localhost:" + PORT))
