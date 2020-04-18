ef01328121747355e71ca5a7b370dea6b9e23b60
Will Hofstadter <whof@bu.edu>
2020-04-18 08:12:54 -0400

    added static file caching

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const path = require('path')
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-const routes = require("./routing/apiroutes");
 require('dotenv').config()
+const PORT = process.env.PORT || 3001;
+const routes = require("./routing/apiroutes");
+const options = { maxAge: 864000000 }
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("client/build", options));
 }
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
