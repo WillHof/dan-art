3518ce5735013ed016eaf459f035d1ac162e6344
Will Hofstadter <whof@bu.edu>
2020-08-13 17:07:47 -0400

    fixed hnav links

--- client/src/components/h-Nav.js
+++ client/src/components/h-Nav.js
@@ -13,26 +13,26 @@ export class Nav extends Component {
                     <div className="collapse navbar-collapse quicksand" id="navbarTogglerDemo02">
                         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
 
-                            <li className={window.location.pathname === "/Paintings" ? "nav-item dropdown-toggler active" : "nav-item dropdown-toggler"}>
+                            <li className={window.location.pathname === "/Main/Paintings" ? "nav-item dropdown-toggler active" : "nav-item dropdown-toggler"}>
                                 <span className="nav-link dropdown-toggle" id="navbarDropdown" type="button" data-toggle="collapse" data-target="#dropmenuHorizontal" aria-haspopup="true" aria-expanded="false">
                                     WORK
                             </span>
                                 <div className="collapse" aria-labelledby="navbarDropdown" id="dropmenuHorizontal">
-                                    <Link to="/Paintings?category=LargeAbstractions" className={window.location.search === "?category=LargeAbstractions" ? "dropdown-item" : "dropdown-item tgray4"} >Large Abstractions</Link>
-                                    <Link to="/Paintings?category=SmallAbstractions" className={window.location.search === "?category=SmallAbstractions" ? "dropdown-item" : "dropdown-item tgray4"}>Small Abstractions</Link>
-                                    <Link to="/Paintings?category=RecentWork" className={window.location.search === "?category=RecentWork" ? "dropdown-item" : "dropdown-item tgray4"}>Recent Work</Link>
-                                    <Link to="/Paintings?category=EarlyWork" className={window.location.search === "?category=EarlyWork" ? "dropdown-item" : "dropdown-item tgray4"} >Early Work <i>(1970-1990)</i></Link>
-                                    {/* <Link to="/Paintings?category=MonotypesDrawings" className={window.location.search === "?category=MonotypesDrawings" ? "dropdown-item" : "dropdown-item tgray4"} >Drawings</Link> */}
-                                    <Link to="/Paintings?category=InstallationViews" className={window.location.search === "?category=InstallationViews" ? "dropdown-item" : "dropdown-item tgray4"} >Installation Views</Link>
+                                    <Link to="/Main/Paintings?category=LargeAbstractions" className={window.location.search === "?category=LargeAbstractions" ? "dropdown-item" : "dropdown-item tgray4"} >Large Abstractions</Link>
+                                    <Link to="/Main/Paintings?category=SmallAbstractions" className={window.location.search === "?category=SmallAbstractions" ? "dropdown-item" : "dropdown-item tgray4"}>Small Abstractions</Link>
+                                    <Link to="/Main/Paintings?category=RecentWork" className={window.location.search === "?category=RecentWork" ? "dropdown-item" : "dropdown-item tgray4"}>Recent Work</Link>
+                                    <Link to="/Main/Paintings?category=EarlyWork" className={window.location.search === "?category=EarlyWork" ? "dropdown-item" : "dropdown-item tgray4"} >Early Work <i>(1970-1990)</i></Link>
+                                    {/* <Link to="/Main/Paintings?category=MonotypesDrawings" className={window.location.search === "?category=MonotypesDrawings" ? "dropdown-item" : "dropdown-item tgray4"} >Drawings</Link> */}
+                                    <Link to="/Main/Paintings?category=InstallationViews" className={window.location.search === "?category=InstallationViews" ? "dropdown-item" : "dropdown-item tgray4"} >Installation Views</Link>
                                 </div>
                             </li>
-                            <li className={window.location.pathname === "/About" ? "nav-item active" : "nav-item"}>
-                                <Link to="/About" className="nav-link">
+                            <li className={window.location.pathname === "/Main/About" ? "nav-item active" : "nav-item"}>
+                                <Link to="/Main/About" className="nav-link">
                                     ABOUT THE ARTIST
                                 </Link>
                             </li>
-                            <li className={window.location.pathname === "/Contact" ? "nav-item active" : "nav-item"}>
-                                <Link to="/Contact" className="nav-link">
+                            <li className={window.location.pathname === "/Main/Contact" ? "nav-item active" : "nav-item"}>
+                                <Link to="/Main/Contact" className="nav-link">
                                     CONTACT
                                 </Link>
                             </li>
